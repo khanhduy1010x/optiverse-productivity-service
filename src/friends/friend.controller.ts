e4dbbfbbe202a1070f590545a11f2f8f48ce7a1c
caotai <votai12315@gmail.com>
2025-05-25 23:37:58 +0700

    OPTV-17 feat:add manage friend

--- src/friends/friend.controller.ts
+++ src/friends/friend.controller.ts
@@ -21,5 +21,111 @@ import { UserDto } from 'src/user-dto/user.dto';
 @ApiBearerAuth('access-token')
 @Controller('/friend')
 export class FriendController {
-  
+  constructor(private readonly friendService: FriendService) {}
+
+  @ApiParam({
+    name: 'userId',
+    type: String,
+  })
+  @Get('user/:userId')
+  async getFriendsByUserID(@Param('userId') userId: string): Promise<ApiResponse<Friend[]>> {
+    const friends = await this.friendService.getFriendsByUserID(userId);
+    return new ApiResponse<Friend[]>(friends);
+  }
+
+  @ApiBody({ type: CreateFriendRequest })
+  @Post('create')
+  async createFriend(
+    @Body() createFriendDto: CreateFriendRequest,
+  ): Promise<ApiResponse<FriendResponse>> {
+    const friend = await this.friendService.createFriend(createFriendDto);
+    return new ApiResponse<FriendResponse>(friend);
+  }
+
+  @ApiParam({
+    name: 'id',
+    type: String,
+  })
+  @ApiBody({ type: UpdateFriendRequest })
+  @Put('update/:id')
+  async updateFriend(
+    @Param('id') friendId: string,
+    @Body() updateFriendDto: UpdateFriendRequest,
+  ): Promise<ApiResponse<FriendResponse>> {
+    const friend = await this.friendService.updateFriend(friendId, updateFriendDto);
+    return new ApiResponse<FriendResponse>(friend);
+  }
+
+  @ApiParam({
+    name: 'id',
+    type: String,
+  })
+  @Delete('delete/:id')
+  async deleteFriend(@Param('id') friendId: string): Promise<ApiResponse<void>> {
+    await this.friendService.deleteFriend(friendId);
+    return new ApiResponse<void>();
+  }
+
+  @ApiParam({
+    name: 'email',
+    type: String,
+  })
+  @Get('public/search-user/:email')
+  async searchUserByEmail(@Param('email') email: string): Promise<ApiResponse<any>> {
+    const user = await this.friendService.searchUserByEmail(email);
+    return new ApiResponse(user);
+  }
+
+  @ApiParam({
+    name: 'friendId',
+    type: String,
+  })
+  @Post('add/:friendId')
+  async addFriend(
+    @Request() req,
+    @Param('friendId') friendId: string,
+  ): Promise<ApiResponse<Friend>> {
+    const user = req.user as UserDto;
+
+    const friend = await this.friendService.addFriend(user.userId, friendId);
+    return new ApiResponse(friend);
+  }
+
+  @ApiParam({
+    name: 'id',
+    type: String,
+  })
+  @Put('accept/:id')
+  async acceptFriend(@Param('id') id: string): Promise<ApiResponse<Friend>> {
+    const updatedFriend = await this.friendService.acceptFriend(id);
+    return new ApiResponse(updatedFriend);
+  }
+
+  @Get('view-all/pending')
+  async viewAllPending(@Request() req): Promise<ApiResponse<Friend[]>> {
+    const user = req.user as UserDto;
+
+    const friends = await this.friendService.viewAllPending(user.userId.toString());
+    return new ApiResponse(friends);
+  }
+
+  @ApiParam({
+    name: 'userId',
+    type: String,
+  })
+  @Get('view-all/:userId')
+  async viewAllFriends(@Param('userId') userId: string): Promise<ApiResponse<Friend[]>> {
+    const friends = await this.friendService.viewAllFriends(userId);
+    return new ApiResponse(friends);
+  }
+
+  @ApiParam({
+    name: 'id',
+    type: String,
+  })
+  @Delete('/:id')
+  async removeFriend(@Param('id') id: string): Promise<ApiResponse<Friend>> {
+    const removed = await this.friendService.removeFriend(id);
+    return new ApiResponse(removed);
+  }
 }
--- src/friends/friend.repository.ts
+++ src/friends/friend.repository.ts
@@ -11,5 +11,80 @@ import { UserHttpClient } from 'src/http-axios/user-http.client';
 
 @Injectable()
 export class FriendRepository {
+  constructor(
+    @InjectModel(Friend.name) private readonly friendModel: Model<Friend>,
+    private readonly userHttpClient: UserHttpClient
+
+  ) {}
+
+  async getFriendsByUserID(userId: string): Promise<Friend[]> {
+    return await this.friendModel.find({ user_id: new Types.ObjectId(userId) }).exec();
+  }
+
+  async createFriend(createFriendDto: CreateFriendRequest): Promise<Friend> {
+    const newFriend = new this.friendModel({
+      ...createFriendDto,
+      user_id: new Types.ObjectId(createFriendDto.user_id),
+      friend_id: new Types.ObjectId(createFriendDto.friend_id),
+    });
+    return await newFriend.save();
+  }
+
+  async updateFriend(friend_id: string, updateFriendDto: UpdateFriendRequest): Promise<Friend> {
+    return await this.friendModel
+      .findByIdAndUpdate(friend_id, updateFriendDto, { new: true })
+      .orFail(new AppException(ErrorCode.NOT_FOUND));
+  }
+
+  async deleteFriend(friend_id: string): Promise<void> {
+    const result = await this.friendModel.deleteOne({ _id: friend_id }).exec();
+    if (result.deletedCount === 0) {
+      throw new AppException(ErrorCode.NOT_FOUND);
+    }
+  }
+
+  async searchUserByEmail(email: string): Promise<UserDto | null> {
+    const response = await this.userHttpClient.getUser(email);
+    if (response.data.code != 1000) {
+      return response.data as UserDto;
+    } else {
+      console.log('User not found');
+      throw new AppException(ErrorCode.NOT_FOUND);
+    }
+  }
+
+
   
+
+  async addFriend(user_id: string, friend_id: string): Promise<Friend | null> {
+    return this.friendModel.create({ user_id, friend_id, status: 'pending' });
+  }
+
+  async acceptFriend(id: string): Promise<Friend | null> {
+    return this.friendModel
+      .findOneAndUpdate(
+        {
+          _id: id,
+        },
+        { status: 'accepted' },
+        { new: true },
+      )
+      .exec();
+  }
+
+  async viewAllFriends(user_id: string): Promise<Friend[]> {
+    return this.friendModel
+      .find({ user_id: new Types.ObjectId(user_id), status: 'accepted' })
+      .exec();
+  }
+
+  async viewAllPending(user_id: string): Promise<Friend[]> {
+    return this.friendModel
+      .find({ friend_id: new Types.ObjectId(user_id), status: 'pending' })
+      .exec();
+  }
+
+  async removeFriend(id: string): Promise<Friend | null> {
+    return this.friendModel.findOneAndDelete({ _id: id }).exec();
+  }
 }
