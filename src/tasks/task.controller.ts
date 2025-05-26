51a6d819067c2957acee232548c912aafd358e92
khanghp11 <Khanghpce170373@fpt.edu.vn>
2025-05-26 13:45:55 +0700

    OPTV-10 Manage Task
    -view task
    -create task
    -update task
    -delete task

--- src/tasks/task.controller.ts
+++ src/tasks/task.controller.ts
@@ -32,5 +32,81 @@ import { UserDto } from 'src/user-dto/user.dto';
 @ApiExtraModels(ApiResponseWrapper, TaskResponse)
 @Controller('/task')
 export class TaskController {
-  
+  constructor(private readonly taskService: TaskService) {}
+
+  @Get('all')
+  async getAllTaskUser(@Request() req): Promise<ApiResponseWrapper<GetAllTaskReponse>> {
+    const user = req.user as UserDto;
+    const result = await this.taskService.getAllTaskByID(user.userId);
+    return new ApiResponseWrapper<GetAllTaskReponse>(result);
+  }
+  @Get(':id')
+  async getTaskById(@Param('id') taskId: string): Promise<ApiResponseWrapper<TaskResponse>> {
+    const task = await this.taskService.getTaskByID(taskId);
+    return new ApiResponseWrapper<TaskResponse>(task);
+  }
+
+  @ApiBody({ type: CreateTaskRequest })
+  @ApiOkResponse({
+    description: 'Create task successfully',
+    schema: {
+      allOf: [
+        { $ref: getSchemaPath(ApiResponseWrapper) },
+        {
+          type: 'object',
+          properties: {
+            data: { $ref: getSchemaPath(TaskResponse) },
+          },
+        },
+      ],
+    },
+  })
+  @ApiResponse({ status: 401, description: 'Created failed' })
+  @Post('')
+  async createTask(
+    @Request() req,
+    @Body() createTaskDto: CreateTaskRequest,
+  ): Promise<ApiResponseWrapper<TaskResponse>> {
+    const user = req.user as UserDto;
+    const task = await this.taskService.createTask(user.userId, createTaskDto);
+    return new ApiResponseWrapper<TaskResponse>(task);
+  }
+
+  @ApiParam({
+    name: 'id',
+    type: String,
+  })
+  @ApiBody({ type: UpdateTaskRequest })
+  @ApiOkResponse({
+    description: 'Update task successfully',
+    schema: {
+      allOf: [
+        { $ref: getSchemaPath(ApiResponseWrapper) },
+        {
+          type: 'object',
+          properties: {
+            data: { $ref: getSchemaPath(TaskResponse) },
+          },
+        },
+      ],
+    },
+  })
+  @Put('/:id')
+  async updateTask(
+    @Param('id') taskId: string,
+    @Body() updateTaskDto: UpdateTaskRequest,
+  ): Promise<ApiResponseWrapper<TaskResponse>> {
+    const task = await this.taskService.updateTask(taskId, updateTaskDto);
+    return new ApiResponseWrapper<TaskResponse>(task);
+  }
+
+  @ApiParam({
+    name: 'id',
+    type: String,
+  })
+  @Delete('/:id')
+  async deleteTask(@Param('id') taskId: string): Promise<ApiResponseWrapper<void>> {
+    await this.taskService.deleteTask(taskId);
+    return new ApiResponseWrapper<void>(null);
+  }
 }
--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -9,5 +9,45 @@ import { CreateTaskRequest } from './dto/request/CreateTaskRequest.dto';
 import { UpdateTaskRequest } from './dto/request/UpdateTaskRequest.dto';
 @Injectable()
 export class TaskRepository {
-  
+  constructor(@InjectModel(Task.name) private readonly taskModel: Model<Task>) {}
+
+  async getAllTaskByID(id: string): Promise<GetAllTaskReponse> {
+    const listTask = await this.taskModel
+      .find({ user_id: new Types.ObjectId(id) })
+      .populate({ path: 'tags', populate: { path: 'tag' } })
+      .orFail(new AppException(ErrorCode.NOT_FOUND));
+    return new GetAllTaskReponse(listTask);
+  }
+  async getTaskByID(taskId: string): Promise<Task> {
+    return await this.taskModel
+      .findById(new Types.ObjectId(taskId))
+      .populate({ path: 'tags', populate: { path: 'tag' } })
+      .orFail(new AppException(ErrorCode.NOT_FOUND));
+  }
+
+  async createTask(userId: string, createTaskDto: CreateTaskRequest): Promise<Task> {
+    const newTask = new this.taskModel({
+      ...createTaskDto,
+      user_id: new Types.ObjectId(userId),
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+    return await newTask.save();
+  }
+
+  async updateTask(taskId: string, updateTaskDto: UpdateTaskRequest): Promise<Task> {
+    return await this.taskModel
+      .findByIdAndUpdate(new Types.ObjectId(taskId), updateTaskDto, { new: true })
+      .populate({ path: 'tags', populate: { path: 'tag' } })
+      .orFail(new AppException(ErrorCode.NOT_FOUND));
+  }
+
+  async deleteTask(taskId: string): Promise<Task> {
+    const task = await this.taskModel.findByIdAndDelete(taskId).exec();
+    if (!task) {
+      throw new AppException(ErrorCode.NOT_FOUND);
+    }
+
+    return task;
+  }
 }
