ea0d561c4638d7011d5c9bbc488964c924532d01
khanghp11 <Khanghpce170373@fpt.edu.vn>
2025-05-26 13:51:03 +0700

    OPTV-11 Manage flashcard deck
    -view flashcard deck
    -create flashcard deck
    -update flashcard deck
    -delete flashcard deck

--- src/flashcard-decks/flashcard-deck.controller.ts
+++ src/flashcard-decks/flashcard-deck.controller.ts
@@ -20,5 +20,62 @@ import { UserDto } from 'src/user-dto/user.dto';
 @ApiBearerAuth('access-token')
 @Controller('/flashcard-deck')
 export class FlashcardDeckController {
-  
+  constructor(private readonly flashcardDeckService: FlashcardDeckService) {}
+
+  @Get('all')
+  async getFlashcardDecksByUserID(@Request() req): Promise<ApiResponse<FlashcardDeck[]>> {
+    const user = req.user as UserDto;
+
+    const flashcardDecks = await this.flashcardDeckService.getFlashcardDecksByUserID(user.userId);
+    return new ApiResponse<any[]>(flashcardDecks);
+  }
+
+  @Get(':id')
+  async getFlashcardDeckById(
+    @Param('id') flashcardDeckId: string,
+  ): Promise<ApiResponse<FlashcardDeck | null>> {
+    const flashcardDeck = await this.flashcardDeckService.getFlashcardDeckById(flashcardDeckId);
+    return new ApiResponse<any>(flashcardDeck);
+  }
+
+  @ApiBody({ type: CreateFlashcardDeckRequest })
+  @Post('')
+  async createFlashcardDeck(
+    @Request() req,
+    @Body() createFlashcardDeckDto: CreateFlashcardDeckRequest,
+  ): Promise<ApiResponse<FlashcardDeckResponse>> {
+    const user = req.user as UserDto;
+    const flashcardDeck = await this.flashcardDeckService.createFlashcardDeck(
+      createFlashcardDeckDto,
+      user.userId,
+    );
+    return new ApiResponse<FlashcardDeckResponse>(flashcardDeck);
+  }
+
+  @ApiParam({
+    name: 'id',
+    type: String,
+  })
+  @ApiBody({ type: UpdateFlashcardDeckRequest })
+  @Patch('/:id')
+  async updateFlashcardDeck(
+    @Param('id') flashcardDeckId: string,
+    @Body() updateFlashcardDeckDto: UpdateFlashcardDeckRequest,
+  ): Promise<ApiResponse<FlashcardDeckResponse>> {
+    const flashcardDeck = await this.flashcardDeckService.updateFlashcardDeck(
+      flashcardDeckId,
+      updateFlashcardDeckDto,
+    );
+    return new ApiResponse<FlashcardDeckResponse>(flashcardDeck);
+  }
+
+  @ApiParam({
+    name: 'id',
+    type: String,
+  })
+  @Delete('/:id')
+  async deleteFlashcardDeck(@Param('id') flashcardDeckId: string): Promise<ApiResponse<void>> {
+    await this.flashcardDeckService.deleteFlashcardDeck(flashcardDeckId);
+    return new ApiResponse<void>(null);
+  }
 }
--- src/flashcard-decks/flashcard-deck.service.ts
+++ src/flashcard-decks/flashcard-deck.service.ts
@@ -9,5 +9,45 @@ import { FlashcardRepository } from '../flashcards/flashcard.repository';
 
 @Injectable()
 export class FlashcardDeckService {
-  
+  constructor(
+    private readonly flashcardDeckRepository: FlashcardDeckRepository,
+    private readonly flashcardService: FlashcardService,
+  ) {}
+
+  async getFlashcardDecksByUserID(userId: string): Promise<FlashcardDeckResponse[]> {
+    return await this.flashcardDeckRepository.getFlashcardDecksByUserID(userId);
+  }
+
+  async getFlashcardDeckById(deckId: string): Promise<FlashcardDeckResponse | null> {
+    return await this.flashcardDeckRepository.getFlashcardDeckById(deckId);
+  }
+
+  async createFlashcardDeck(
+    createFlashcardDeckDto: CreateFlashcardDeckRequest,
+    userId: string,
+  ): Promise<FlashcardDeckResponse> {
+    const flashcardDeck = await this.flashcardDeckRepository.createFlashcardDeck(
+      createFlashcardDeckDto,
+      userId,
+    );
+    return new FlashcardDeckResponse(flashcardDeck);
+  }
+
+  async updateFlashcardDeck(
+    flashcardDeckId: string,
+    updateFlashcardDeckDto: UpdateFlashcardDeckRequest,
+  ): Promise<FlashcardDeckResponse> {
+    const flashcardDeck = await this.flashcardDeckRepository.updateFlashcardDeck(
+      flashcardDeckId,
+      updateFlashcardDeckDto,
+    );
+    return new FlashcardDeckResponse(flashcardDeck);
+  }
+
+  async deleteFlashcardDeck(flashcardDeckId: string): Promise<void> {
+    const flashcards = await this.flashcardService.getFlashcardsByDeckID(flashcardDeckId);
+    const ids = flashcards.map((flashcard) => flashcard._id.toString());
+    await this.flashcardService.deleteManyByIds(ids);
+    await this.flashcardDeckRepository.deleteFlashcardDeck(flashcardDeckId);
+  }
 }
