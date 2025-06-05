0840d20b3efbe2f54d95c401d89d3cd2a2b0b5f7
khanghp11 <Khanghpce170373@fpt.edu.vn>
2025-06-05 13:17:23 +0700

    OPTV-10 CRUD Task Task-Tag Tag

--- src/tags/tag.controller.ts
+++ src/tags/tag.controller.ts
@@ -25,5 +25,57 @@ import { UserDto } from 'src/user-dto/user.dto';
 @ApiExtraModels(ApiResponseWrapper, TagResponse)
 @Controller('/tag')
 export class TagController {
-  
+  constructor(private readonly tagService: TagService) {}
+
+  @Get('all')
+  async getAllTagsUser(@Request() req): Promise<ApiResponseWrapper<Tag[]>> {
+    const user = req.userInfo as UserDto;
+    const tags = await this.tagService.getAllTagsByUserID(user.userId);
+    return new ApiResponseWrapper<Tag[]>(tags);
+  }
+
+  @ApiParam({
+    name: 'id',
+    type: String,
+  })
+  @Get(':id')
+  async getTagById(@Param('id') tagId: string): Promise<ApiResponseWrapper<TagResponse>> {
+    const tag = await this.tagService.getTagByID(tagId);
+    return new ApiResponseWrapper<TagResponse>(tag);
+  }
+
+  @ApiBody({ type: CreateTagRequest })
+  @Post('')
+  async createTag(
+    @Request() req,
+    @Body() createTagDto: CreateTagRequest,
+  ): Promise<ApiResponseWrapper<TagResponse>> {
+    const user = req.userInfo as UserDto;
+    const tag = await this.tagService.createTag(user.userId, createTagDto);
+    return new ApiResponseWrapper<TagResponse>(tag);
+  }
+
+  @ApiParam({
+    name: 'id',
+    type: String,
+  })
+  @ApiBody({ type: UpdateTagRequest })
+  @Put(':id')
+  async updateTag(
+    @Param('id') tagId: string,
+    @Body() updateTagDto: UpdateTagRequest,
+  ): Promise<ApiResponseWrapper<TagResponse>> {
+    const tag = await this.tagService.updateTag(tagId, updateTagDto);
+    return new ApiResponseWrapper<TagResponse>(tag);
+  }
+
+  @ApiParam({
+    name: 'id',
+    type: String,
+  })
+  @Delete(':id')
+  async deleteTag(@Param('id') tagId: string): Promise<ApiResponseWrapper<void>> {
+    await this.tagService.deleteTag(tagId);
+    return new ApiResponseWrapper<void>(null);
+  }
 }
